a5b8309cc2c2838d4f7e378c5cbaf50734b8a035
Vitali Ahel <nonsens_bill@mail.ru>
2022-08-17 21:54:18 +0300

    finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,31 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        }
 
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
-
+        this.candidate = Math.round((this.right-this.left) / 2) + this.left ; 
+         
+        return this.candidate
     }
 
     lower() {
-
+        
+        this.right = this.candidate;
+        
     }
 
     greater() {
-
+        
+        this.left = this.candidate ;
+        
     }
 }
+
+
 
 module.exports = GuessingGame;
